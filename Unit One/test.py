9d892d899fbfb9e2b8428bc3590b623185b85011
ujwwal <ujjwalguptamail@gmail.com>
2025-07-29 11:49:03 +0530

    list operations

--- Unit One/test.py
+++ Unit One/test.py
@@ -1,4 +1,4 @@
-user_input = input("Enter a list of numbers seperated by spaced")
+user_input = input("Enter a list of numbers seperated by spaces: \n")
 
 #conveert the input string to a list of strings
 input_list = user_input.split()
@@ -15,3 +15,8 @@ length = len(numbers)
 #sort the list
 sorted_numbers = sorted(numbers)
 
+#outputs
+print(f"original list: {numbers}")
+print(f"sum of numbers: {total}")
+print(f"number of elements: {length}")
+print(f"sorted lsit of numbers {sorted_numbers}")
