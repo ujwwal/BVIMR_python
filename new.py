02b775d03e06f71c1fe63a16a150cd28a2ed654e
ujwwal <ujjwalguptamail@gmail.com>
2025-08-05 12:29:49 +0530

    Add new scripts for array manipulation and set operations

---
+++ new.py
@@ -0,0 +1,7 @@
+import numpy as np
+a = np.arange(10)
+b =  a[1:5]
+b[0] = 10
+print(a)
+
+print(b)
---
+++ nope.py
@@ -0,0 +1,12 @@
+# s = {1,2,4,5}
+# l = []
+
+# for i in range(len(s)):
+#     l +=[1+i]
+# print(l)
+
+
+a = {1,'two,',3.0,3,27.5,'four',5}
+b = {27.5,10}
+
+print(a | b)
