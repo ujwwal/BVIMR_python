d6e40aae0ff0994778ac41b2ab30c1444559eb14
ujwwal <ujjwalguptamail@gmail.com>
2025-08-07 21:18:13 +0530

    test

--- nope.py
+++ nope.py
@@ -1,3 +1,4 @@
+import numpy as np
 # s = {1,2,4,5}
 # l = []
 
@@ -6,7 +7,27 @@
 # print(l)
 
 
-a = {1,'two,',3.0,3,27.5,'four',5}
-b = {27.5,10}
+# a = {1,'two,',3.0,3,27.5,'four',5}
+# b = {27.5,10}
 
-print(a | b)
+# print(a | b)
+
+# d = {1:"python", 2:[1,2,3]}
+# # d[2].append[4]
+# x=d[0]
+# d["one"]=1
+# d.update({'one':2})
+
+# name = "hello"
+# print(name.capitalize())
+
+
+# a = np.array([range(i, i+4) for i in [1,3,5]])
+# print(a)
+
+product = ['pen']
+price = [5]
+brand = ["faber castel"]
+stationery = [product,price,brand]
+stationery[0].insert(0,'notebook')
+print(stationery)
