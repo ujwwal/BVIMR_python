74381fd989f7556e7b77fe6be8752cf5976f5bd6
ujwwal <ujjwalguptamail@gmail.com>
2025-08-21 10:47:04 +0530

    add: implement while loop demonstration with continue statement

--- Class Work/Unit One/contd_demonstration.py
+++ Class Work/Unit One/contd_demonstration.py
@@ -4,3 +4,5 @@ for num in range(1, 11):
     if num % 2 == 0:
         continue  # Skip even numbers
     print(f"Odd number: {num}")
+
+print("code written by ujjwal gupta - 0231bca051 of bca 5th sem bvimr")
---
+++ Class Work/Unit One/contd_while_demonstration.py
@@ -0,0 +1,8 @@
+count = 0
+while count<10:
+    count+=1
+    if count%2 ==0:
+        continue
+    print(count)
+
+print("code written by ujjwal gupta - 0231bca051 of bca 5th sem bvimr")
